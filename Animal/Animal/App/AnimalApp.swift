d57a5cc5b64197be3f8dec2147c81a69e7780e5e
jhson <sjh@payprotocol.io>
2024-05-27 16:16:13 +0900

    feat: 프로젝트 초기 셋팅

---
+++ Animal/Animal/App/AnimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalApp.swift
+//  Animal
+//
+//  Created by sonjuhyeong on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animal/Animal/View/ContentView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Animal
+//
+//  Created by sonjuhyeong on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .hLeading()
+        .vTop()
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
