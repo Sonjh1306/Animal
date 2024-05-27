05cd80ffe3cdfb8dec2e2fe6dbcc1e2440edbbad
jhson <sjh@payprotocol.io>
2024-05-27 17:37:58 +0900

    feat: HomeView 구현

--- Animal/Animal/App/HomeView.swift
+++ Animal/Animal/App/HomeView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            // 1. ContentView
+            ContentView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
+                }
+            
+            // 2. VideoView
+            VideoView()
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("비디오")
+                }
+            
+            // 3. GalleryView
+            GalleryView()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("갤러리")
+                }
+            
+            // 4. AppInfoView
+            AppInfoView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("앱정보")
+                }
+        }
     }
 }
 
---
+++ Animal/Animal/View/AppInfo/AppInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  AppInfoView.swift
+//  Animal
+//
+//  Created by sonjuhyeong on 5/27/24.
+//
+
+import SwiftUI
+
+struct AppInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppInfoView()
+}
